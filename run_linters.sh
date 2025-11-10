e6e47fdfece94d1960e99712d0a07d021e729941
Anton Pershin <tony.pershin@gmail.com>
2025-11-10 15:46:36 +0300

    Fixed linter run commands (wrong project name)

--- run_linters.sh
+++ run_linters.sh
@@ -1,11 +1,11 @@
-black project_name/
+black lecture_me/
 
-isort project_name/
+isort lecture_me/
 
 printf "\nPress any key to continue to pylint...\n"
 read -n 1 -s -r
-pylint project_name/
+pylint lecture_me/
 
 printf "\nPress any key to continue to mypy...\n"
 read -n 1 -s -r
-mypy project_name/
+mypy lecture_me/
